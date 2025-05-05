610ad9eff805deb32d67124e56fb534669f55d92
FelixStillLearning <felixangga077@gmail.com>
2025-05-05 17:29:29 +0700

    error part 999

--- genecare-platform/app/crypto/__init__.py
+++ genecare-platform/app/crypto/__init__.py
@@ -1,5 +1,5 @@
 from .aes import AESCipher, encrypt_health_data, decrypt_health_data, encrypt_dna_data, decrypt_dna_data
-from .key_management import generate_key, load_key
+from .key_management import get_key_manager, derive_key_from_password
 
 __all__ = ['AESCipher', 'encrypt_health_data', 'decrypt_health_data', 
-           'encrypt_dna_data', 'decrypt_dna_data', 'generate_key', 'load_key']
+           'encrypt_dna_data', 'decrypt_dna_data', 'get_key_manager', 'derive_key_from_password']
